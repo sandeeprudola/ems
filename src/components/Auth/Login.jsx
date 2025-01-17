5ddf778d413a2dd924262e9da98d03d033c73525
sandeeprudola <sandeeprudola>
2025-01-17 09:35:44 +0530

    date-16jan2025 part2

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+
+    const SubmitHandler=(e)=>{
+      e.preventDefault()
+      console.log("email is:",email)
+      console.log("password is:",password)
+
+      setemail('')
+      setpassword('')
+
+    }
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
         <div className=' rounded-xl border-2 border-emerald-600 p-20'>
 
-            <form className='flex flex-col items-center justify-center'>
-                <input required className='border-2 border-emerald-600 px-5 py-3 text-xl rounded-full outline-none bg-transparent placeholder:text-gray-300' type="email" placeholder='Enter Your Email' />
-                <input className='border-2 border-emerald-600 px-5 py-3 text-xl rounded-full outline-none bg-transparent placeholder:text-gray-300 mt-4' type="password" placeholder='Password' />
+            <form onSubmit={(e)=>{
+
+              SubmitHandler(e)
+
+            }} className='flex flex-col items-center justify-center'>
+                <input required 
+                  value={email}
+                  onChange={(e)=>{
+                    setemail(e.target.value)
+                }}
+                className='border-2 border-emerald-600 px-5 py-3 text-xl rounded-full outline-none bg-transparent placeholder:text-gray-300' type="email" placeholder='Enter Your Email'
+                />
+                <input 
+                  value={password}
+                  onChange={(e)=>{
+                    setpassword(e.target.value)
+                  }}
+                className='border-2 border-emerald-600 px-5 py-3 text-xl rounded-full outline-none bg-transparent placeholder:text-gray-300 mt-4' type="password" placeholder='Password' 
+                />
                 <button className='mt-7 border-none outline-none bg-emerald-600 px-5 py-3 text-xl rounded-full text-white'>Log in</button>
             </form>
 
