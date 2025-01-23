2f92577567ce2842f94c0b112ce0ee997f7d5c1b
sandeeprudola <sandeeprudola>
2025-01-23 21:48:48 +0530

    date-23jan2025 part2

--- src/components/TaskList/TaskSwipe.jsx
+++ src/components/TaskList/TaskSwipe.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const TaskSwipe = () => {
   return (
-    <div className='h-[55%] bg-red-500 w-full mt-10'>
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex flex-nowrap gap-5 items-center justify-start py-5 w-full mt-10'>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
+
+        </div>
+        
+
+        
+        
 
     </div>
   )
