c4bb2a5ce7b1787f595877f5dc2860445da98663
sandeeprudola <sandeeprudola10@gmail.com>
2025-01-23 23:07:48 +0530

    date-23jan2025

--- src/components/TaskList/TaskSwipe.jsx
+++ src/components/TaskList/TaskSwipe.jsx
@@ -3,24 +3,13 @@ import React from 'react'
 const TaskSwipe = () => {
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto flex flex-nowrap gap-5 items-center justify-start py-5 w-full mt-10'>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
+        <div className=' flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+            <div className='flex justify-between px-3 py-1 rounded'>
+            <h3 className='bg-red-600'>High</h3>
+            <h4 className=''>23JAN2025</h4>
+            </div>
         </div>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
-        <div className=' flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-
-        </div>
+    
         
         
 
